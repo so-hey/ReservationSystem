9843da8f206ac5013f63d12776bcc7ed372c92b6
so-hey <soh.tngt@gmail.com>
2026-04-21 02:01:47 +0900

    1週間前から予約可能に変更．予約可能時間を20:00までに変更

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,18 +1,18 @@
 import { AvailableTime } from '@/shared/types';
 
 /**
- * 1日先から30日先までの日付リストを返す
+ * 1週間先から1ヶ月先までの日付リスト（土日除外）を返す
  */
-export const DATE_UNTIL_NEXT_MONTH: string[] = Array.from({ length: 90 }, (_, i) => {
+export const DATE_UNTIL_NEXT_MONTH: string[] = Array.from({ length: 24 }, (_, i) => {
   const date = new Date();
-  date.setDate(date.getDate() + i + 1);
+  date.setDate(date.getDate() + i + 7);
   return date;
 })
   .filter((date) => date.getDay() !== 0 && date.getDay() !== 6)
   .map((date) => date.toISOString().split('T')[0]);
 
-// 利用可能な時間テーブル（09:00〜17:30, 30分刻み）
-export const TIME_TABLE: AvailableTime[] = Array.from({ length: 18 }, (_, i) => {
+// 利用可能な時間テーブル（09:00〜20:00, 30分刻み）
+export const TIME_TABLE: AvailableTime[] = Array.from({ length: 22 }, (_, i) => {
   const startHour = 9 + Math.floor((i * 30) / 60);
   const startMinute = (i * 30) % 60;
   const endHour = 9 + Math.floor((i * 30 + 30) / 60);
